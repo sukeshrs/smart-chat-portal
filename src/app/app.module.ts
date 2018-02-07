b72d3614a19f1572da402a2a1dff0a9ad8944a0b
sukeshrs <sukesrs@gmail.com>
2018-02-06 17:58:38 -0800

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 import { AppFooterComponent } from './app-footer/app-footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
 
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'login', component: LoginComponent }
+ ];
 
 @NgModule({
   declarations: [
     AppComponent,
     AppNavbarComponent,
-    AppFooterComponent
+    AppFooterComponent,
+    HomePageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(routes, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
