0f86362ff9c1bfd7751168f3a5641a3556ca5139
victoriaagb <victoriaagb93@gmail.com>
2018-02-13 18:48:26 -0500

    adding service for api New York Times API

--- src/app/app-navbar/app-navbar.component.ts
+++ src/app/app-navbar/app-navbar.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs/Observable';
+
+//TODO: Map response to class
+// interface News {
+//     webUrl: string;
+//     snippet:string;
+//     blog:string;
+//     source:string;
+// }
 
 @Component({
   selector: 'app-navbar',
@@ -7,11 +17,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppNavbarComponent implements OnInit {
 
-  isCollapsed : boolean;
-  constructor() { }
+  isCollapsed: boolean;
+  searchInput: string;
+  articles$: Observable<string>;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.isCollapsed =true;
+    this.isCollapsed = true;
+    this.searchInput = '';
   }
 
+  // Call to service
+  onSubmitSearch(): void {
+    console.log('searching for ' + this.searchInput);
+    console.log('Data Source: ' + this.dataService.newsSource);
+      this.dataService.getDataToday(this.searchInput).subscribe(
+        data => {
+          this.articles$ = data;
+          console.log(this.articles$);
+        }
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Routes, RouterModule } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
@@ -9,6 +12,7 @@ import { AppFooterComponent } from './app-footer/app-footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
 
+import { DataService } from './data.service';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
@@ -27,9 +31,15 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
-    RouterModule.forRoot(routes, {useHash: true})
+    RouterModule.forRoot(routes, {useHash: true}),
+    FormsModule,
+    HttpClientModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    DataService,
+    HttpClientModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
