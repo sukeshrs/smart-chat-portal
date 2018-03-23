c485081e15ad5df3918b83d784a60e6494895435
sukeshrs <sukesrs@gmail.com>
2018-03-23 16:18:41 -0700

    Updated the analytics component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Routes, RouterModule } from "@angular/router";
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {HttpModule} from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
@@ -13,12 +14,15 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
 import { DataService } from './app-navbar/data.service';
 import { ModelService } from './model/model.service';
+import { AnalyticsComponent } from './analytics/analytics.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'home', component: HomePageComponent },
-  { path: 'login', component: LoginComponent }
- ];
+  { path: 'login', component: LoginComponent },
+  { path: 'analytics', component: AnalyticsComponent },
+
+];
 
 @NgModule({
   declarations: [
@@ -27,15 +31,18 @@ const routes: Routes = [
     AppFooterComponent,
     HomePageComponent,
     LoginComponent,
+    AnalyticsComponent,
   ],
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
-    RouterModule.forRoot(routes, {useHash: true}),
+    RouterModule.forRoot(routes, { useHash: true }),
+    MDBBootstrapModule.forRoot(),
     FormsModule,
     HttpClientModule,
     HttpModule
   ],
+  schemas: [NO_ERRORS_SCHEMA],
   providers: [
     ModelService,
     DataService,
