94c7c12089ced257404793c2059950fb191bd796
sukeshrs <sukesrs@gmail.com>
2018-04-01 13:13:01 -0700

    Added module for config bot

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
 import { AnalyticsComponent } from './analytics-feature/analytics/analytics.component';
+import { BotConfigComponent } from './bot-config-feature/bot-config/bot-config.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'analytics', component: AnalyticsComponent },
+  { path: 'configure', component: BotConfigComponent },
 
 ];
 
