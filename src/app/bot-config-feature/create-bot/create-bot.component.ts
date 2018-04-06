3364b1c7ca3ba6e81e4baf7de8f3384538fa34b2
sukeshrs <sukesrs@gmail.com>
2018-04-06 16:39:52 -0700

    Added the name your bot component

---
+++ src/app/bot-config-feature/create-bot/create-bot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'create-bot',
+  templateUrl: './create-bot.component.html',
+  styleUrls: ['./create-bot.component.scss']
+})
+export class CreateBotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
