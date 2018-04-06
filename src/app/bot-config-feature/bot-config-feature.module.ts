20e7d9ff54509c554624bb60690070719762c0c9
sukeshrs <sukesrs@gmail.com>
2018-04-05 17:20:36 -0700

    Added shared module

--- src/app/bot-config-feature/bot-config-feature.module.ts
+++ src/app/bot-config-feature/bot-config-feature.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { BotConfigComponent } from './bot-config/bot-config.component';
 import { DisplayBotComponent } from './display-bot/display-bot.component';
 import { ConfigureComponent } from './configure/configure.component';
+import { SharedFeatureModule } from '../shared-feature/shared-feature.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    SharedFeatureModule
   ],
   declarations: [BotConfigComponent, DisplayBotComponent, ConfigureComponent]
 })
