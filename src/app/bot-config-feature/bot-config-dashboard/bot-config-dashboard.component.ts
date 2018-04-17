d7429131a5b5f871b3869ee0f221a7c389e49479
sukeshrs <sukesrs@gmail.com>
2018-04-16 20:00:53 -0700

    Updated to convert json string to object

--- src/app/bot-config-feature/bot-config-dashboard/bot-config-dashboard.component.ts
+++ src/app/bot-config-feature/bot-config-dashboard/bot-config-dashboard.component.ts
@@ -15,15 +15,21 @@ export class BotConfigDashboardComponent implements OnInit {
   ngOnInit() {
       this.getBotConfigList();
   }
-  getBotConfigList(){
+  getBotConfigList() {
     this.botConfigService.getBotConfigList().subscribe(
       data => {
         this.botConfigList = data;
+        this.botConfigList.forEach(function (entry) {
+          console.log("entry" + entry);
+          let string = JSON.stringify(entry.value);
+          let temp =JSON.parse(string);
+          let value = JSON.parse(temp);
+          entry.value = value;
+        });
         console.log(this.botConfigList);
       },
       error => console.log("ERROR ::" + error)
     );
-
   }
 
 }
