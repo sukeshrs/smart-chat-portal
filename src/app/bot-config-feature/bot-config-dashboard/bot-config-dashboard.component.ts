75aafcbe3b0e4860097e4fe1153809fd417df3f8
victoriaagb <victoriaagb93@gmail.com>
2018-04-19 19:28:05 -0400

    commented out workaround

--- src/app/bot-config-feature/bot-config-dashboard/bot-config-dashboard.component.ts
+++ src/app/bot-config-feature/bot-config-dashboard/bot-config-dashboard.component.ts
@@ -19,13 +19,13 @@ export class BotConfigDashboardComponent implements OnInit {
     this.botConfigService.getBotConfigList().subscribe(
       data => {
         this.botConfigList = data;
-        this.botConfigList.forEach(function (entry) {
-          console.log("entry" + entry);
-          let string = JSON.stringify(entry.value);
-          let temp =JSON.parse(string);
-          let value = JSON.parse(temp);
-          entry.value = value;
-        });
+        // this.botConfigList.forEach(function (entry) {
+        //   console.log("entry" + entry);
+        //   let string = JSON.stringify(entry.value);
+        //   let temp =JSON.parse(string);
+        //   let value = JSON.parse(temp);
+        //   entry.value = value;
+        // });
         console.log(this.botConfigList);
       },
       error => console.log("ERROR ::" + error)
