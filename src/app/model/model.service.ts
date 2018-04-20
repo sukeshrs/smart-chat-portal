dd5f1e4bf2a92f7aed563cae9038aa99ca4b3aba
sukeshrs <sukesrs@gmail.com>
2018-04-19 18:18:07 -0700

    Updated dashboard actions

--- src/app/model/model.service.ts
+++ src/app/model/model.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Account } from "./account.model";
 import { Analytics } from "../analytics-feature/model/analytics.model";
+import { BotConfigRespository } from "../bot-config-feature/model/bot-config-repository.model";
 
 @Injectable()
 export class ModelService {
   accountResponse: Account[];
   analyticsResponse: Analytics;
   customer_id : string = '99999999';
-  constructor() { }
+  botConfigRespository : BotConfigRespository[];
+  currentBot : BotConfigRespository;
+  constructor() {
+
+   }
 
 }
